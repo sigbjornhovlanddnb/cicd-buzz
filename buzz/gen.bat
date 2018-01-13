e8aaa09bc180696c7b40ab5fe3302efbcdb891fc
Hovland <AB30961@dnbnor.net>
2018-01-13 23:12:21 +0100

    Initial commit

---
+++ buzz/gen.bat
@@ -0,0 +1,8 @@
+echo "1"
+C:\Users\AB30961\AppData\Local\Programs\Python\Python36-32\python.exe generator.py
+echo "2"
+C:\Users\AB30961\AppData\Local\Programs\Python\Python36-32\python.exe generator.py
+echo "3"
+C:\Users\AB30961\AppData\Local\Programs\Python\Python36-32\python.exe generator.py
+echo "4"
+C:\Users\AB30961\AppData\Local\Programs\Python\Python36-32\python.exe generator.py
