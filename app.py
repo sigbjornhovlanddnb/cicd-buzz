62ed687af716fb2295fa0371d7742fe91ea2f11c
sigbjornhovlandnb <sigbjorn.hovland@dnb.no>
2018-01-14 17:37:47 +0100

    Test

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 def generate_buzz(): 
     buzz = generator.generate_buzz()
     return render_template('index.html', buzz=buzz) 
+	
  
 if __name__ == "__main__": 
     app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000))) 
